cadda5f88789c4a08af681cf6c1e9bb624d8c5c1
OliverWoodwardSky <146005493+OliverWoodwardSky@users.noreply.github.com>
2023-09-27 12:37:00 +0100

    Update app.py
    edits made

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'This is the home page'
+    return 'This is the home page, I have made an edit'
 
 @app.route('/about')
 def about():
-    return 'This is the about page'
+    return 'This is the about page. this is cool.'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
